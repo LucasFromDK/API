2fbec3be3ad614d98ba974670a094f2e64a4cad8
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-05-11 17:35:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!' + " " + Math.random())
+  let randomNum = Math.floor(Math.random() * 10 + 1)
+
+  if (randomNum < 5) {
+    res.send(`Hello World! \n${randomNum} < 5`)
+  } else if (randomNum > 5) {
+    res.send(`Hello World! \n${randomNum} > 5`)
+  } else if (randomNum == 5) {
+    res.send(`Hello World! \n${randomNum} is 5`)
+  }
+
 })
 
 app.listen(port, () => {
